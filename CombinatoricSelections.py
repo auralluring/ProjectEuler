8c1c923f4e739e1db3423cd940ea57f8fe421b1b
HorseLuvver <45722265+HorseLuvver@users.noreply.github.com>
2020-09-09 16:00:43 -0600

    Update and rename CombinatoricSelections.py to [53] [done] CombinatoricSelections.py

--- CombinatoricSelections.py
+++
@@ -1,8 +0,0 @@
-from math import factorial
-aboveOneMillion = 0
-for n in range(23, 101):
-    for r in range(2, n):
-        s = factorial(n)/ (factorial(r) * factorial(n - r))
-        if s > 1000000:
-            aboveOneMillion += 1
-print(aboveOneMillion)
