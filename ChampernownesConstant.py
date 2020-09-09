d291fd72d397e78d0db9cdfc20c498de23bd435f
HorseLuvver <45722265+HorseLuvver@users.noreply.github.com>
2020-09-09 15:34:51 -0600

    Update and rename ChampernownesConstant.py to [40] [done] ChampernownesConstant.py

--- ChampernownesConstant.py
+++
@@ -1,5 +0,0 @@
-constant = ""
-for i in range(1, 1000000):
-    constant += str(i)
-print(int(constant[0]) * int(constant[9]) * int(constant[99]) * int(constant[999]) * int(constant[9999]) * int(constant[99999]) * int(constant[999999]))
-#done!
