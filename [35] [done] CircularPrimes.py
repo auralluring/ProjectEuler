016418badc663061000d28c4ea9372c23b1adabd
HorseLuvver <45722265+HorseLuvver@users.noreply.github.com>
2020-09-09 15:39:23 -0600

    Update and rename CircularPrimes.py to [35] [done] CircularPrimes.py

--- [35] [done] CircularPrimes.py
+++ [35] [done] CircularPrimes.py
@@ -1,3 +1,11 @@
+"""
+The number, 197, is called a circular prime because all rotations of the digits: 197, 971, and 719, are themselves prime.
+
+There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+
+How many circular primes are there below one million?
+"""
+
 from sympy import primerange, isprime
 def rotations(n):
     i = 1
@@ -17,3 +25,5 @@ for n in primes:
         if all(perm in primes for perm in perms):
             circles += perms
 print(len(circles))
+
+#done!
